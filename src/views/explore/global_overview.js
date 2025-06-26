dff8502d07be1a320e52fc64ef6ec20e93ccf875
Dana Allen <danaallen@Danas-MacBook-Air.local>
2025-06-26 08:56:25 +0100

    Adding filters to global overview map

--- src/views/explore/global_overview.js
+++ src/views/explore/global_overview.js
@@ -1,13 +1,18 @@
-import React, { useState, useEffect } from "react"
+import React, { useState, useCallback, useEffect } from "react"
 import { Tooltip as ReactTooltip } from "react-tooltip";
-import { GlobalMap, useApiEndpoint, useErrorHandler, useLoadingWheelHandler } from '@centre-for-virus-research/gdb-core-package';
+import { Button, ButtonGroup } from 'react-bootstrap';
+import { GlobalMap, useFilterParams,  submitApiQuery, useApiEndpoint, useErrorHandler, useLoadingWheelHandler, SequencesFilter } from '@centre-for-virus-research/gdb-core-package';
 
-const GlobalOverview = () => {
+const GlobalOverview = ({ filters=null }) => {
 
-  const url = "/api/statistics/get_global_distribution_of_sequences/"
-  const { endpointData, isPending, endpointError } = useApiEndpoint(url);
+  const [params, setParams] = useFilterParams(filters);
+  const url = params ? "/api/statistics/get_global_distribution_of_sequences/" : 
+                        "/api/statistics/get_global_distribution_of_sequences/";
+  const { endpointData, isPending, endpointError } = useApiEndpoint(url, params);
 
   const [content, setContent] = useState("");
+  const [data, setData] = useState([])
+  const [showFilter, setShowFilter] = useState(false);
   // setTooltipContent shows the black bubble with country name and sequences
   // It has to go in here. Not sure why but it doesn't work anywhere else
 
@@ -17,12 +22,34 @@ const GlobalOverview = () => {
   useEffect(() => {
     triggerLoadingWheel(isPending)
     if (endpointError) triggerError(endpointError)
+      if (endpointData) setData(endpointData);
   }, [endpointError, isPending]);
+
+  const handleData = async (data) => {
+    if (data.status === 200){
+        const json = await data.json();  
+        setData(json)
+    }
+}
+  const handleFiltersChange = useCallback((data) => {
+      setParams(data)
+      const queryString = new URLSearchParams(data).toString();
+      const fullUrl = `${process.env.REACT_APP_BACKEND_URL}${'/api/statistics/get_global_distribution_of_sequences/'}${queryString ? `?${queryString}` : ''}`;
+      submitApiQuery(fullUrl, false, handleData)
+      setShowFilter(false);
+  }, []);
 
   return (
     <div class='container'>
       <h2>Global Sequences Overview</h2>
       <p>Select country or clade to see more information.</p>
+
+      <div className='col right-align' >
+        <ButtonGroup>
+            <Button className="paging-buttons" onClick={() => setShowFilter(true)}> Filters </Button>
+        </ButtonGroup>   
+      </div>
+      <SequencesFilter show={showFilter} onFilterSelect={handleFiltersChange} onClose={() => setShowFilter(false)}/>
 
       <GlobalMap data={endpointData} setTooltipContent={setContent}></GlobalMap>
       <ReactTooltip id="map-tooltip">{content}</ReactTooltip>
